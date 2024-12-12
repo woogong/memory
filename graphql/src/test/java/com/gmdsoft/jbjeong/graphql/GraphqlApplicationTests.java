185eca7fcb8c38cd93d42313ba29e0e95fde3eff
정재봉 <jbjeong@gmdsoft.com>
2024-12-12 15:45:51 +0900

    graphql 맛보기 프로젝트 추가

---
+++ graphql/src/test/java/com/gmdsoft/jbjeong/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdsoft.jbjeong.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
